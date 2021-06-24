8ca491d778ade4733dae0b82ebde281e22f73e95
Силкин Дмитрий Валерьевич <DSilkin@inno.local>
2021-06-24 15:18:15 +0300

    Смена библиотеки и первый тест

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -9,10 +9,10 @@ public class MyFirstProgram {
       hello("Bob");
 
     Square s = new Square(5);
-        System.out.println("Площадь квадрата со стороной " +  s.l + " = " + area(s));
+        System.out.println("Площадь квадрата со стороной " +  s.l + " = " + s.area());
 
     Rectangle r = new Rectangle(4, 6);
-    System.out.println("Площадь прямоугольника со сторонами " +  r.a + " и " + r.b + " = " + area(r));
+    System.out.println("Площадь прямоугольника со сторонами " +  r.a + " и " + r.b + " = " + r.area());
 
 
 
@@ -23,14 +23,8 @@ public class MyFirstProgram {
     System.out.println("Hello, " + somebody + "!");
   }
 
-  public static double area(Square s) {
 
-    return s.l * s.l;
-  }
 
-  public static double area(Rectangle r) {
 
-    return r.a * r.b;
-  }
 
 }
